4b32794761541c76fc66a57d9089864ed22629b7
vvr3ddy <vvr-rockstar@bmsit.in>
2021-01-03 16:10:54 +0530

    :octocat: implement Update of CRUD
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- server.js
+++ server.js
@@ -1,30 +1,95 @@
-const express= require('express');
-const mongoose= require('mongoose');
-const bodyParser= require('body-parser');
-const port=8000;
-const app= express();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const port = 8000;
+const app = express();
+
+const User = require('./models/User');
+mongoose.connect('mongodb://localhost/userData')
 
 app.use(bodyParser.json());
 
-app.listen(port, ()=>{
-	console.log(`server is listening on port:${port}`)
+app.listen(port, () => {
+  console.log(`server is listening on port:${port}`)
 })
 
 // CREATE
-app.post('/users',(req,res)=>{
-  // User.create()
+app.post('/users', (req, res) => {
+  User.create(
+    {
+      name: req.body.newData.name,
+      email: req.body.newData.email,
+      password: req.body.newData.password
+    },
+    (err, data) => {
+      if (err) {
+        res.json({ success: false, message: err })
+      } else if (!data) {
+        res.json({ success: false, message: "Not Found" })
+      } else {
+        res.json({ success: true, data: data })
+      }
+    })
 })
 
 app.route('/users/:id')
-// READ
-.get((req,res)=>{
-  // User.findById()
-})
-// UPDATE
-.put((req,res)=>{
-  // User.findByIdAndUpdate()
-})
-// DELETE
-.delete((req,res)=>{
-  // User.findByIdAndDelete()
-})
+  // READ
+  .get((req, res) => {
+    User.findById(req.params.id, (err, data) => {
+      if (err) {
+        res.json({
+          success: false,
+          message: err
+        })
+      }
+      else if (!data) {
+        res.json({
+          success: false,
+          message: "Not Found"
+        })
+      }
+      else {
+        res.json({
+          success: true,
+          message: data
+        })
+      }
+    })
+  })
+
+  // UPDATE
+  .put((req, res) => {
+    User.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: req.body.newData.name,
+        email: req.body.newData.email,
+        password: req.body.newData.password
+      },
+      {
+        new: true
+      },
+      (err, data) => {
+        if (err) {
+          res.json({
+            success: false,
+            message: err
+          })
+        } else if (!data) {
+          res.json({
+            success: false,
+            message: "Not Found"
+          })
+        } else {
+          res.json({
+            success: true,
+            data: data
+          })
+        }
+      }
+    )
+  })
+  // DELETE
+  .delete((req, res) => {
+    // User.findByIdAndDelete()
+  })
